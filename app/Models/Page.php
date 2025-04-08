5dadcbced681eac921b5a2f047d7698718dee55b
Raúl Sebastián <info@raulsebastian.es>
2025-04-08 10:00:55 +0200

    Añado Flexible contet block a Filament

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -6,20 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Statikbe\FilamentFlexibleContentBlocks\Models\Concerns\HasPageAttributesTrait;
 
 class Page extends Model
 {
-  use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, HasPageAttributesTrait;
 
-  protected $fillable = [
-    'title',
-    'slug',
-    'published_at',
-  ];
+    protected $fillable = [
+        'title',
+        'slug',
+        'published_at',
+    ];
 
-  public function contents(): HasMany
-  {
-    return $this->hasMany(PageContent::class);
+    public function contents(): HasMany
+    {
+        return $this->hasMany(PageContent::class);
 
-  }
+    }
 }
