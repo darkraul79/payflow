6ce8159015ba03f439181c6fc74ec020278f10aa
Raúl Sebastián <info@raulsebastian.es>
2025-04-23 19:29:47 +0200

    FET-1 Crear Backend Cambio campo content en posts

--- database/migrations/2025_04_23_080441_create_posts_table.php
+++ database/migrations/2025_04_23_080441_create_posts_table.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('slug')->unique();
-            $table->string('content')->nullable();
+            $table->text('content')->nullable();
             $table->string('resume')->nullable();
             $table->dateTime('date')->nullable();
             $table->text('address')->nullable();
