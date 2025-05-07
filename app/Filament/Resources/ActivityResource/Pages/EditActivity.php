7170888bcdae8151c00b40e3eebea98da61d66db
Raúl Sebastián <info@raulsebastian.es>
2025-05-07 13:22:12 +0200

    FET-1 Crear Backend Añado botones de visitar y guardar al header de la edición de Actividades, Noticias y Proyectos

--- app/Filament/Resources/ActivityResource/Pages/EditActivity.php
+++ app/Filament/Resources/ActivityResource/Pages/EditActivity.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\ActivityResource\Pages;
 
 use App\Filament\Resources\ActivityResource;
+use App\Models\Activity;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ForceDeleteAction;
 use Filament\Actions\RestoreAction;
@@ -15,9 +17,18 @@ class EditActivity extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+
             DeleteAction::make(),
             ForceDeleteAction::make(),
             RestoreAction::make(),
+            Action::make('visit')
+                ->label('Visitar')
+                ->url(fn (?Activity $record) => $record->getUrl() ?? null)
+                ->icon('heroicon-o-arrow-top-right-on-square')
+                ->openUrlInNewTab()
+                ->color('success')
+                ->visible(config('filament-fabricator.routing.enabled')),
+            Action::make('Guardar')->action('save'),
         ];
     }
 }
--- app/Filament/Resources/NewsResource/Pages/EditNews.php
+++ app/Filament/Resources/NewsResource/Pages/EditNews.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\NewsResource\Pages;
 
 use App\Filament\Resources\NewsResource;
+use App\Models\News;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ForceDeleteAction;
 use Filament\Actions\RestoreAction;
@@ -18,6 +20,14 @@ class EditNews extends EditRecord
             DeleteAction::make(),
             ForceDeleteAction::make(),
             RestoreAction::make(),
+            Action::make('visit')
+                ->label('Visitar')
+                ->url(fn (?News $record) => $record->getUrl() ?? null)
+                ->icon('heroicon-o-arrow-top-right-on-square')
+                ->openUrlInNewTab()
+                ->color('success')
+                ->visible(config('filament-fabricator.routing.enabled')),
+            Action::make('Guardar')->action('save'),
         ];
     }
 }
