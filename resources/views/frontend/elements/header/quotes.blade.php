4c8b3fdd9f75ed8b9949e214f7edda50ce875e16
Raúl Sebastián <info@raulsebastian.es>
2025-05-12 13:27:01 +0200

    FET-1 Crear Backend Añado blade ui kit e iconos de musica a blockquotes

--- resources/views/frontend/elements/header/quotes.blade.php
+++ resources/views/frontend/elements/header/quotes.blade.php
@@ -11,7 +11,9 @@
         <blockquote
             class="mx-auto flex w-fit items-center rounded-t-2xl bg-white px-8 py-4 text-center text-2xl leading-6 font-bold text-pretty shadow-[0px_0px_12px_0px_rgba(0,0,0,0.25)] md:text-[2.5rem] md:leading-10 lg:px-40 lg:py-7"
         >
+            <x-bi-music-note class="fill-azul-wave-100 mx-2 inline h-12 w-12" />
             "{{ $p->blockquotes->first()->text }}"
+            <x-bi-music-note class="fill-azul-wave-100 mx-2 inline h-12 w-12" />
         </blockquote>
     @endif
 </div>
