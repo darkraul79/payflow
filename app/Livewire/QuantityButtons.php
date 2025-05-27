90d0fd4c714faf31ef5deac1530fd6061ce56d48
Raúl Sebastián <info@raulsebastian.es>
2025-05-27 18:41:00 +0200

    FET-5 Integración Redsys Refactor QuantityButtons.php

--- app/Livewire/QuantityButtons.php
+++ app/Livewire/QuantityButtons.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Product;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class QuantityButtons extends Component
@@ -11,82 +12,57 @@ class QuantityButtons extends Component
 
     public Product $product;
 
-
     public bool|string $errorMessage = '';
+
     public bool $size;
 
-    public function mount(Product $product, int $quantity = 1, string $size = "normal"): void
+    public function mount(Product $product, int $quantity = 1, string $size = 'normal'): void
     {
         $this->quantity = $quantity;
         $this->product = $product;
 
-        $this->size = ($size == "normal");
+        $this->size = ($size == 'normal');
 
     }
 
-    public function render()
+    public function render(): View
     {
         return view('livewire.quantity-buttons');
     }
 
-    public function add()
+    public function add(): void
     {
-        $this->quantity += 1;
-        if ($this->quantity > $this->product->stock) {
-            $this->errorMessage = 'No hay suficiente stock (' . $this->product->stock . ' max)';
+        $this->updateQuantity($this->quantity + 1);
+    }
+
+    public function substract(): void
+    {
+        $this->updateQuantity($this->quantity - 1);
+    }
+
+    public function update(): void
+    {
+        $this->updateQuantity($this->quantity);
+    }
+
+    private function updateQuantity(int $newQuantity): void
+    {
+        if ($newQuantity > $this->product->stock) {
+            $this->errorMessage = 'No hay suficiente stock ('.$this->product->stock.' max)';
             $this->quantity = $this->product->stock;
-
-            return;
+        } elseif ($newQuantity < 1) {
+            $this->errorMessage = 'No puedes agregar menos de 1 producto';
+            $this->quantity = 1;
+        } else {
+            $this->errorMessage = false;
+            $this->quantity = $newQuantity;
         }
-
-        $this->errorMessage = false;
 
         $this->updateEvent();
     }
 
     public function updateEvent(): void
     {
-
         $this->dispatch('updateQuantity', $this->quantity, $this->product);
-
-    }
-
-    public function update()
-    {
-        if ($this->quantity > $this->product->stock) {
-            $this->errorMessage = 'No hay suficiente stock (' . $this->product->stock . ' max)';
-            $this->quantity = $this->product->stock;
-
-            return;
-        }
-        if ($this->quantity < 1) {
-            $this->errorMessage = 'No puedes agregar menos de 1 producto';
-            $this->quantity = 1;
-
-            return;
-        }
-
-        $this->errorMessage = false;
-
-        $this->updateEvent();
-    }
-
-    public function substract()
-    {
-        $this->quantity -= 1;
-        if ($this->quantity < 1) {
-            $this->errorMessage = 'No puedes agregar menos de 1 producto';
-            $this->quantity = 1;
-
-            return;
-        }
-        $this->errorMessage = false;
-
-        $this->updateEvent();
-    }
-
-    public function checkStock()
-    {
-        
     }
 }
--- tests/Feature/CartTest.php
+++ tests/Feature/CartTest.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Livewire\CardProduct;
+use App\Livewire\CartButtonComponent;
 use App\Livewire\PageCartComponent;
 use App\Livewire\ProductAddCart;
 use App\Livewire\QuantityButtons;
 use App\Models\Product;
 use App\Services\Cart;
+
 use function Pest\Livewire\livewire;
 
 test('puedo añadir producto a carrito desde la página de producto', function () {
@@ -13,7 +15,6 @@ test('puedo añadir producto a carrito desde la página de producto', function (
     $producto = Product::factory()->create([
         'stock' => 5,
     ]);
-
 
     livewire(QuantityButtons::class, [
         'product' => $producto,
@@ -52,7 +53,6 @@ test('suma correctamente el número de artículos', function () {
 });
 
 test('puedo añadir producto a carrito desde la página de productos', function () {
-
 
     $producto = Product::factory()->create([
         'stock' => 5,
@@ -110,4 +110,37 @@ test('si vació cesta no aparece en el icono superior', function () {
         ->assertDispatched('updatedCart');
     $this->get(route('cart'))
         ->assertDontSeeHtml('id="cart-count-badge"');
+});
+test('si actulizo cantidad de producto en carrito se muestra en icono superio', function () {
+    $producto = Product::factory()->create([
+        'stock' => 5,
+    ]);
+
+    livewire(CardProduct::class, [
+        'product' => $producto,
+    ])->call('addToCart', $producto);
+
+    $this->get(route('cart'))
+        ->assertSeeHtml('id="cart-count-badge"');
+
+    livewire(QuantityButtons::class, [
+        'product' => $producto,
+    ])->call('add')
+        ->assertDispatched('updateQuantity', 2)
+        ->assertSet('quantity', 2);
+
+    livewire(PageCartComponent::class)
+        ->call('updateQuantity', 2, $producto)
+        ->assertDispatched('updatedCart');
+
+    livewire(CartButtonComponent::class)
+        ->assertSet('quantity', 2);
+
+    $this->get(route('cart'))
+        ->assertSeeHtmlInOrder([
+            '<div
+            id="cart-count-badge"',
+            '2',
+            '</div>',
+        ]);
 });
