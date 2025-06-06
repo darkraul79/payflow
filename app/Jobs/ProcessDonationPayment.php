ecb8930f6a7b462c1930ed8732c55aec93dd4691
Raúl Sebastián <info@raulsebastian.es>
2025-06-06 09:37:34 +0200

    FET-5 Arreglo problema con actividades de footer

---
+++ app/Jobs/ProcessDonationPayment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Donation;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class ProcessDonationPayment implements ShouldQueue
+{
+    use Queueable;
+
+    private Donation $donation;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(Donation $donation)
+    {
+        //
+        $this->donation = $donation;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $this->donation->processPay();
+    }
+}
