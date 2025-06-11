9f7944b08803d1d136901c92a6544d1da83f718c
Raúl Sebastián <info@raulsebastian.es>
2025-06-11 13:41:10 +0200

    FET-5 Nuevos texto páginas de OK y KO de donación y pedido

--- resources/views/donation/ko.blade.php
+++ resources/views/donation/ko.blade.php
@@ -1,15 +1,23 @@
 @extends('components.filament-fabricator.layouts.default')
 
 @section('static')
-    <section>
-        <header class="mb-12 text-center">
+    <section
+        class="mx-auto flex min-h-[450px] flex-col items-center justify-center"
+    >
+        <header class="mb-12 flex flex-row items-center gap-5 text-center">
+            <div
+                class="border-error bg-error-50 inline-block rounded-full border-8 bg-white ring-8"
+            >
+                <x-bi-exclamation-lg class="text-azul-marino h-6 w-6" />
+            </div>
             <h1 class="title">{{ $page->title }}</h1>
         </header>
-        <div class="mb-12 h-40 text-center">
-            <h2 class="subtitle">
-                Ha ocurrido un error al procesar la donación
-            </h2>
-            <p>Por favor ponte en contacto con nosotros.</p>
+        <div class="mb-12 text-center">
+            <h2 class="subtitle">No se ha podido completar tu donación.</h2>
+            <p>
+                Aún estás a tiempo de impulsar la ola solidaria. Vuelve a
+                intentarlo o contáctanos si necesitas ayuda.
+            </p>
         </div>
     </section>
 @endsection
--- resources/views/donation/ok.blade.php
+++ resources/views/donation/ok.blade.php
@@ -1,16 +1,20 @@
 @extends('components.filament-fabricator.layouts.default')
 
 @section('static')
-    <section>
-        <header class="mb-12 text-center">
+    <section
+        class="mx-auto flex min-h-[450px] flex-col items-center justify-center"
+    >
+        <header class="mb-12 flex flex-row items-center gap-5 text-center">
+            <div
+                class="border-success bg-success-50 inline-block rounded-full border-8 bg-white ring-8"
+            >
+                <x-bi-check-lg class="text-azul-marino h-6 w-6" />
+            </div>
             <h1 class="title">{{ $page->title }}</h1>
         </header>
-        <div class="mb-12 h-screen text-center">
-            <h2 class="subtitle">Gracias por la donación</h2>
-            <p>
-                Your order has been successfully placed. We will process it
-                shortly.
-            </p>
+        <div class="mb-12 text-center">
+            <h2 class="subtitle">¡Gracias por tu donación!</h2>
+            <p>Juntos hacemos que la ola solidaria no deje de avanzar.</p>
         </div>
     </section>
 @endsection
