460b5060e822526f16e796daa3253394fa951e4f
Raúl Sebastián <info@raulsebastian.es>
2025-06-11 19:08:26 +0200

    FET-5 Ajuste flotante donación móviles

--- resources/views/components/modal-donation.blade.php
+++ resources/views/components/modal-donation.blade.php
@@ -10,7 +10,7 @@
             x-trap.inert.noscroll="modalIsOpen"
             x-on:keydown.esc.window="modalIsOpen = false"
             x-on:click.self="modalIsOpen = false"
-            class="fixed inset-0 z-30 flex items-end justify-center bg-black/70 p-4 pb-8 sm:items-center lg:p-8"
+            class="fixed inset-0 z-30 flex items-end justify-center overflow-auto bg-black/70 p-4 pb-8 sm:items-center lg:p-8"
             role="dialog"
             aria-modal="true"
             aria-labelledby="defaultModalTitle"
@@ -22,7 +22,7 @@
                 x-transition:enter="transition delay-100 duration-200 ease-out motion-reduce:transition-opacity"
                 x-transition:enter-start="scale-110 opacity-0"
                 x-transition:enter-end="scale-100 opacity-100"
-                class=""
+                class="top-[100px]"
             >
                 <div
                     class="z-50 flex w-full flex-col items-end justify-end p-4 lg:max-w-xl"
