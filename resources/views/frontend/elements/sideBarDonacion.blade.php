b25d84de883b08b870a13468d8c9b654153c3bdb
Raúl Sebastián <info@raulsebastian.es>
2025-06-12 12:26:06 +0200

    FET-5 Arreglo error en sidebar con banner de donación

--- resources/views/frontend/elements/sideBarDonacion.blade.php
+++ resources/views/frontend/elements/sideBarDonacion.blade.php
@@ -1,5 +1,5 @@
 @if ($page->donacion)
     <div class="flex w-full items-start justify-end md:w-2/6">
-        @livewire('donacion-banner')
+        <livewire:donacion-banner prefix="sidebar" wire:key="sidebar" />
     </div>
 @endif
