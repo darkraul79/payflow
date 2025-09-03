5daa4e4f9eb512a62f53501d871ad33571f1231d
Raúl Sebastián <info@raulsebastian.es>
2025-09-03 16:14:39 +0200

    Actualizo schedule para ejecutar queue:work cada minuto

--- routes/console.php
+++ routes/console.php
@@ -4,3 +4,6 @@
 use App\Console\Commands\GetPaymentsOfMonth;
 
 Schedule::command(GetPaymentsOfMonth::class)->timezone('Europe/Madrid')->monthlyOn(5, '08:00');
+Schedule::command('queue:work --stop-when-empty')
+    ->everyMinute()
+    ->withoutOverlapping();
