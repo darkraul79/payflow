aafa7ed15e1163715c2b5a695de668a75f246ebf
Raúl Sebastián <info@raulsebastian.es>
2025-11-12 12:39:04 +0100

    Corrijo error de migracion al eliminar campo taxes

--- database/migrations/2025_11_11_093740_delete_taxes_field_orders.php
+++ database/migrations/2025_11_11_093740_delete_taxes_field_orders.php
@@ -4,18 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
-        Schema::table('Orders', function (Blueprint $table) {
+        Schema::table('orders', function (Blueprint $table) {
             $table->dropColumn('taxes');
         });
     }
 
     public function down(): void
     {
-        Schema::table('Orders', function (Blueprint $table) {
+        Schema::table('orders', function (Blueprint $table) {
             //
             $table->float('taxes')->default(0.00);
         });
